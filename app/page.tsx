5aeb7f13633b8536b7bf17e1394278c00a9b52f4
Autoro <alexdmiesbauer@gmail.com>
2025-04-07 00:11:50 -0700

    Changing content of page to test Github Actions automatic deployment.

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
         priority
       />
       <h1 className="text-4xl font-bold mt-4">Hello from Miesbaua</h1>
+      <h1 className="text-4xl font-bold mt-4">This was deployed automatically!</h1>
     </div>
   );
 }
